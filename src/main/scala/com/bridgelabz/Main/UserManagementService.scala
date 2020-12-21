80421fd4e66e2896b02607cc55a86cc2a0be1432
RuchirDixit <ruchirtd96@gmail.com>
2020-12-21 12:23:17 +0530

    [Ruchir]Refactored: Added comments for methods

--- src/main/scala/com/bridgelabz/Main/UserManagementService.scala
+++ src/main/scala/com/bridgelabz/Main/UserManagementService.scala
@@ -2,10 +2,20 @@ package com.bridgelabz.Main
 
 class UserManagementService {
 
+  /**
+   *
+   * @param loginRequest : Request object containing details of login
+   * @return : If successfull login return "Login Successful"
+   */
   def userLogin(loginRequest: Request): String = {
     "Login Successful"
   }
 
+  /**
+   *
+   * @param createUserRequest : Request object containing details of registering user
+   * @return : If successfull login return "User created"
+   */
   def createUser(createUserRequest: Request):  String = "User Created"
 
   def protectedContent: String  = "This method is secured"
