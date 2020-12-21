b7d2adab2995e85c0344db76a3078e273bccfafe
RuchirDixit <ruchirtd96@gmail.com>
2020-12-21 12:23:58 +0530

    [Ruchir]Added: Case class with email and password fields for register and login

--- src/main/scala/com/bridgelabz/Main/Request.scala
+++ src/main/scala/com/bridgelabz/Main/Request.scala
@@ -1,6 +1,6 @@
 package com.bridgelabz.Main
 import play.api.libs.json.{Json, Reads}
-
+// Request case class having fields email and password
 case class Request(
                          email: String,
                          password: String
