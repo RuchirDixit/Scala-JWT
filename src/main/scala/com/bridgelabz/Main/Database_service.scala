fdcfa7ff8ce07c90cf274e045ed198c11714069a
RuchirDixit <ruchirtd96@gmail.com>
2020-12-24 14:01:11 +0530

    [Ruchir]Added: Database service which saves user and returns all users

---
+++ src/main/scala/com/bridgelabz/Main/Database_service.scala
@@ -0,0 +1,51 @@
+package com.bridgelabz.Main
+
+import org.bson.BsonType
+import org.mongodb.scala.Document
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+object Database_service {
+
+  /**
+   *
+   * @param credentials : Data about the user that is getting stored in database
+   * @return : If data is entered successfully then returns SUCCESS or else return FAILURE
+   */
+  def saveUser(credentials:Request) = {
+    val userToBeAdded : Document = Document("email" -> credentials.email, "password" -> credentials.password)
+    val ifUserExists = checkIfExists(credentials.email)
+    if(ifUserExists == true)
+    {
+      "Failure"
+    }
+    else
+    {
+      val future = MongoDatabase.collection.insertOne(userToBeAdded).toFuture()
+      Await.result(future,10.seconds)
+      "Success"
+    }
+  }
+
+  /**
+   *
+   * @param email : Email id of use to check whether this user already exists
+   * @return : If user already exists it returns true or else it returns false
+   */
+  def checkIfExists(email : String): Boolean = {
+    val data = Await.result(getUsers(),10.seconds)
+    data.foreach(d => d.foreach(obj =>
+      if(obj._2.getBsonType() == BsonType.STRING){
+        if(obj._2.asString().getValue().equals(email))
+          return true
+      }
+    ))
+    false
+  }
+  // Returns All the users present inside database in the form of future
+  def getUsers() = {
+    MongoDatabase.collection.find().toFuture()
+  }
+
+}
