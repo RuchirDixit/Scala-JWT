fae64219d9111d0a1c565a173aa81a09c497d215
RuchirDixit <ruchirtd96@gmail.com>
2020-12-26 13:49:16 +0530

    [Ruchir]Refactored: To login using email and password if correct

--- src/main/scala/com/bridgelabz/Main/UserManagementRoutes.scala
+++ src/main/scala/com/bridgelabz/Main/UserManagementRoutes.scala
@@ -2,19 +2,20 @@ package com.bridgelabz.Main
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import com.typesafe.scalalogging.LazyLogging
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
 
-class UserManagementRoutes(service: UserManagementService) extends PlayJsonSupport {
+class UserManagementRoutes(service: UserManagementService) extends PlayJsonSupport with LazyLogging {
   val routes: Route =
     pathPrefix("user") {
       // for login using post request, returns success on successful login or else returns unauthorized
       path("login") {
         (post & entity(as[Request])) { loginRequest =>
+          logger.info("Login response: " + service.userLogin(loginRequest))
           if (service.userLogin(loginRequest) == "Login Successful") {
-            val token = TokenAuthorization.generateToken(loginRequest.email)
-            complete((StatusCodes.OK, "Success"))
+            complete((StatusCodes.OK, "Successfully logged in!"))
           } else {
-            complete(StatusCodes.Unauthorized)
+            complete(StatusCodes.Unauthorized,"Cannot login")
           }
         }
       } ~
