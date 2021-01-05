0acd56e5af37f9722252d3b70a966bf59ffcdb5d
RuchirDixit <ruchirtd96@gmail.com>
2021-01-05 12:32:50 +0530

    [Ruchir]Refactored: While saving document isVerifed is set to false

--- src/main/scala/com/bridgelabz/Main/Database_service.scala
+++ src/main/scala/com/bridgelabz/Main/Database_service.scala
@@ -4,9 +4,11 @@ import org.bson.BsonType
 import org.mongodb.scala.Document
 import org.mongodb.scala.model.Projections._
 import org.mongodb.scala.model.Filters._
+
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
+import scala.util.matching.Regex
 object Database_service {
 
   /**
@@ -14,18 +16,24 @@ object Database_service {
    * @param credentials : Data about the user that is getting stored in database
    * @return : If data is entered successfully then returns SUCCESS or else return FAILURE
    */
-  def saveUser(credentials:Request) = {
-    val userToBeAdded : Document = Document("email" -> credentials.email, "password" -> credentials.password)
-    val ifUserExists = checkIfExists(credentials.email)
-    if(ifUserExists == true)
-    {
-      "Failure"
+  def saveUser(credentials:User) = {
+    val emailRegexPattern = "^[a-zA-Z0-9+-._]+@[a-zA-Z0-9.-]+$"
+    if(credentials.email.matches(emailRegexPattern)){
+      val userToBeAdded : Document = Document("email" -> credentials.email, "password" -> credentials.password, "isVerified" -> false)
+      val ifUserExists = checkIfExists(credentials.email)
+      if(ifUserExists == true)
+      {
+        "Failure"
+      }
+      else
+      {
+        val future = MongoDatabase.collection.insertOne(userToBeAdded).toFuture()
+        Await.result(future,10.seconds)
+        "Success"
+      }
     }
-    else
-    {
-      val future = MongoDatabase.collection.insertOne(userToBeAdded).toFuture()
-      Await.result(future,10.seconds)
-      "Success"
+    else {
+      "Validation Failed"
     }
   }
 
@@ -49,8 +57,10 @@ object Database_service {
     MongoDatabase.collection.find().toFuture()
   }
 
+  // Returns user that matches with given email ID and excluded ObjectId while returning
   def getUsersUsingFilter(email : String) = {
-    MongoDatabase.collection.find(equal("email",email)).projection(excludeId()).toFuture()
+      println("MongoDatabase:"+MongoDatabase.collection.find())
+      MongoDatabase.collection.find(equal("email",email)).projection(excludeId()).toFuture()
   }
 
 }
