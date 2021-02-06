35a80a7709c6841c04ab45bc9323c6ac4a8e1ce5
RuchirDixit <ruchirtd96@gmail.com>
2021-02-06 13:40:46 +0530

    [Ruchir]Added: Test cases for routes

--- src/test/scala/com/bridgelabz/RegistrationTest.scala
+++ src/test/scala/com/bridgelabz/RegistrationTest.scala
@@ -1,19 +1,46 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.bridgelabz
-// TODO : 1. Check using MongoDatabase.find whether you are able to access data from database;
+
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import akka.routing.Router
-import akka.http.scaladsl.model.{HttpEntity, HttpMethods, HttpRequest, MediaTypes, StatusCodes}
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.model.{HttpEntity, MediaTypes, StatusCodes}
 import akka.util.ByteString
-import com.bridgelabz.Main.{Database_service, MongoDatabase, User, UserManagementRoutes, UserManagementService}
+import com.bridgelabz.Main.routes.UserManagementRoutes
+import com.bridgelabz.Main.services.UserManagementService
 import org.scalatest.matchers.should
 import org.scalatest.wordspec.AnyWordSpec
-import concurrent.duration._
-import scala.concurrent.Await
 class RegistrationTest extends AnyWordSpec with should.Matchers with ScalatestRouteTest {
 
   "A Router" should {
     "register successfully" in {
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |    "email":"ruchirtd103@gmail.com",
+           |    "password":"demo",
+           |    "isVerified":true
+           |}
+              """.stripMargin)
+      Post("/user/register", HttpEntity(MediaTypes.`application/json`, jsonRequest)) ~> new UserManagementRoutes(new UserManagementService).routes ~> check {
+        status shouldBe StatusCodes.OK
+      }
+    }
+  }
+  "A router" should {
+    "register unsuccessful" in {
       val jsonRequest = ByteString(
         s"""
            |{
@@ -23,10 +50,28 @@ class RegistrationTest extends AnyWordSpec with should.Matchers with ScalatestRo
               """.stripMargin)
       Post("/user/register", HttpEntity(MediaTypes.`application/json`,jsonRequest)) ~> new UserManagementRoutes(new UserManagementService).routes ~> check
       {
-        status shouldBe StatusCodes.OK
+        status shouldBe StatusCodes.BadRequest
       }
     }
-    "login successfully" in {
+  }
+  "A router" should {
+    "register with improper email id" in {
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |    "email":"@gmail.com",
+           |    "password":"demo"
+           |}
+              """.stripMargin)
+      Post("/user/register", HttpEntity(MediaTypes.`application/json`,jsonRequest)) ~> new UserManagementRoutes(new UserManagementService).routes ~> check
+      {
+        status shouldBe StatusCodes.BadRequest
+      }
+    }
+  }
+
+  "A router" should {
+    "login unsuccessfully" in {
       val jsonRequest = ByteString(
         s"""
            |{
@@ -38,6 +83,22 @@ class RegistrationTest extends AnyWordSpec with should.Matchers with ScalatestRo
       Post("/user/login", HttpEntity(MediaTypes.`application/json`,jsonRequest)) ~> new UserManagementRoutes(new UserManagementService).routes ~> check
       {
         status shouldBe StatusCodes.UnavailableForLegalReasons
+      }
+    }
+  }
+  "A router" should {
+    "login successfully" in {
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |    "email":"ruchirtd102@gmail.com",
+           |    "password":"d",
+           |    "isVerified":true
+           |}
+              """.stripMargin)
+      Post("/user/login", HttpEntity(MediaTypes.`application/json`,jsonRequest)) ~> new UserManagementRoutes(new UserManagementService).routes ~> check
+      {
+        status shouldBe StatusCodes.InternalServerError
       }
     }
   }
